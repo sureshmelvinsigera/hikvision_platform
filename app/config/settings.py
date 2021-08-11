94a0cdfae9c3bf56040c46ee8494849565a1bb3a
bpro <iserver12345@gmail.com>
2021-08-11 14:18:13 -0400

    fix bug with collectstatic

--- app/config/settings.py
+++ app/config/settings.py
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+TEMPLATE_DIR = [os.path.join(BASE_DIR, 'templates')]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': TEMPLATE_DIR,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
